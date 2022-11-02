2a869f696008db0b3e885065cb7011337446be01
FaiChou <faichou.zh@gmail.com>
2022-11-02 16:37:39 +0800

    Initial Commit

---
+++ uCopy/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  uCopy
+//
+//  Created by 周辉 on 2022/11/2.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ uCopy/uCopyApp.swift
@@ -0,0 +1,17 @@
+//
+//  uCopyApp.swift
+//  uCopy
+//
+//  Created by 周辉 on 2022/11/2.
+//
+
+import SwiftUI
+
+@main
+struct uCopyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
