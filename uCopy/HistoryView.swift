e5588d053c12a2b6ffd6139824b18031b38e3752
FaiChou <faichou.zh@gmail.com>
2022-11-23 10:55:03 +0800

    add keyboard shortcut

--- uCopy/HistoryView.swift
+++ uCopy/HistoryView.swift
@@ -9,7 +9,6 @@ import SwiftUI
 import CoreData
 
 struct HistoryView: View {
-//    @FetchRequest(sortDescriptors: [SortDescriptor(\.createDate, order: .reverse)]) var history: FetchedResults<History>
     @FetchRequest(fetchRequest: HistoryView.fetchRequestWithLimit(size: 20)) var history: FetchedResults<History>
     @Environment(\.managedObjectContext) var context
     @State private var refreshID = UUID()
@@ -27,27 +26,19 @@ struct HistoryView: View {
         VStack {
             ForEach(history.indices.prefix(20), id: \.self) { index in
                 let title = history[index].title ?? "Unknown"
-                Button("\(index+1). \(title.trunc(length: 30))") {
-                    print(title)
+                let indexToShow = index + 1
+                if (index < 9) {
+                    Button("\(indexToShow). \(title.trunc(length: 30))") {
+                        print(title)
+                    }
+                    .keyboardShortcut(KeyEquivalent(Character(UnicodeScalar(0x0030+indexToShow)!)))
+                } else {
+                    Button("\(indexToShow). \(title.trunc(length: 30))") {
+                        print(title)
+                    }
                 }
             }.id(refreshID)
             Divider()
-//            Button("Add") {
-//                let id = UUID()
-//                let title = "\(NSDate.now)"
-//                let type = "text"
-//                let item = History(context: context)
-//                item.id = id
-//                item.title = title
-//                item.type = type
-//                do {
-//                    try context.save()
-//                    refreshID = UUID()
-//                } catch {
-//                    let nserror = error as NSError
-//                    print("Unresolved error \(nserror), \(nserror.userInfo)")
-//                }
-//            }
             Button("Clear History") {
                 for item in history {
                     context.delete(item)
